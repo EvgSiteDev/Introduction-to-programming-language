55770909a735bfa94962497979b132025e354d46
Evgeniy Belousov <EvgSiteDev@yandex.ru>
2022-08-06 22:05:10 +0300

    Выполнена задача номер 8

---
+++ task8/allEvenNumbers.cs
@@ -0,0 +1,24 @@
+﻿string  numString,
+        allEvenNumString = "";
+
+int num;
+
+Console.WriteLine("Все четные числа от 1 до введенного");
+Console.Write("Введите целое число:");
+numString = Console.ReadLine()!;
+num = int.Parse(numString);
+
+for (int counter = 2; counter <= num; counter += 2)
+{
+    allEvenNumString += counter;
+
+    if (counter + 2 > num) {
+        allEvenNumString += ".";        
+    }
+    else {
+        allEvenNumString += ","; 
+    }
+}
+
+
+Console.WriteLine("Четные числа от 1 до " + num + ": " + allEvenNumString);
