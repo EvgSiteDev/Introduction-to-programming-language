902cb04cca45907277a9cf536685210eb449b06b
Evgeniy Belousov <EvgSiteDev@yandex.ru>
2022-08-06 06:44:54 +0300

    Выполнена задача номер 2

---
+++ task2/maxAndMin.cs
@@ -0,0 +1,31 @@
+﻿string  firstNumString,
+        secondNumString;
+
+int firstNum,
+    secondNum;
+
+Console.WriteLine("Нахождение максимального и минимального числа");
+Console.Write("Введите первое число:");
+firstNumString = Console.ReadLine()!;
+firstNum = int.Parse(firstNumString);
+
+Console.Write("Введите второе число:");
+secondNumString = Console.ReadLine()!;
+secondNum = int.Parse(secondNumString);
+
+if (firstNum > secondNum)
+    {
+        Console.WriteLine("Максимальное число:" + firstNumString);
+        Console.WriteLine("Минимальное число:" + secondNumString);
+    }
+
+else 
+    {
+        Console.WriteLine("Максимальное число:" + secondNumString);
+        Console.WriteLine("Минимальное число:" + firstNumString);
+    }
+
+
+
+
+
