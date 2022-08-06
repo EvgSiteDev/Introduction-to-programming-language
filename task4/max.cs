a5ee54951cfeae6a96c6092515cb45f31ebd4509
Evgeniy Belousov <EvgSiteDev@yandex.ru>
2022-08-06 21:12:05 +0300

    Выполнена задача номер 4

---
+++ task4/max.cs
@@ -0,0 +1,41 @@
+﻿string  firstNumString,
+        secondNumString,
+        thirdNumString;
+
+int firstNum,
+    secondNum,
+    thirdNum,
+    maxNum;
+
+Console.WriteLine("Нахождение максимального числа");
+Console.Write("Введите первое число:");
+firstNumString = Console.ReadLine()!;
+firstNum = int.Parse(firstNumString);
+
+Console.Write("Введите второе число:");
+secondNumString = Console.ReadLine()!;
+secondNum = int.Parse(secondNumString);
+
+Console.Write("Введите третье число:");
+thirdNumString = Console.ReadLine()!;
+thirdNum = int.Parse(thirdNumString);
+
+maxNum = firstNum;
+
+if (maxNum < secondNum)
+{
+    maxNum = secondNum;
+}
+if (maxNum < thirdNum)
+{
+    maxNum = thirdNum;
+}
+Console.WriteLine("Максимальное число:" + maxNum);
+
+
+
+
+
+
+
+
