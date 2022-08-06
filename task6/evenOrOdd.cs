3d3221cd6a27b49aedd203217e70ab9a9e499df7
Evgeniy Belousov <EvgSiteDev@yandex.ru>
2022-08-06 21:26:43 +0300

    Выполнена задача номер 6

---
+++ task6/evenOrOdd.cs
@@ -0,0 +1,18 @@
+﻿string  numString;
+
+int num;
+
+Console.WriteLine("Четное или нечетное число");
+Console.Write("Введите целое число:");
+numString = Console.ReadLine()!;
+num = int.Parse(numString);
+int remains = num % 2;
+
+if (remains == 1)
+{
+    Console.WriteLine("Ваше число нечетное");
+}
+else if (remains == 0)
+{
+    Console.WriteLine("Ваше число четное");
+}
