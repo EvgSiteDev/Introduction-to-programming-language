434c0848f34e0c6e38d1650dfc169a387e814287
Evgeniy Belousov <EvgSiteDev@yandex.ru>
2022-08-11 13:39:44 +0300

    Выполнена задача номер 10

---
+++ task10/theSecondDigit.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Поиск третьей цифры в задданном трёхзначном числе");
+Console.WriteLine("_________________________________________________");
+Console.Write("Введите трёхзначное целое число: ");
+string stringNum = Console.ReadLine()!;
+int length = stringNum.Length;
+while (length != 3)
+{
+    if (length != 3)
+    {
+        Console.Write("Вы ввели не трёхзначное число. Введите трёхзначное целое число: ");
+        stringNum = Console.ReadLine()!;
+        length = stringNum.Length;
+    }
+    else if (length == 3)
+    {
+        break;
+    }
+};
+Console.WriteLine("Вторая цифра числа " + stringNum + ": " + stringNum[1]);
