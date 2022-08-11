b63cd11945cb6aeb30e4df472bb9cfa495dd5d45
Evgeniy Belousov <EvgSiteDev@yandex.ru>
2022-08-11 14:53:34 +0300

    Выполнена задача номер 13

---
+++ task13/thirdDigit.cs
@@ -0,0 +1,17 @@
+﻿
+void searchForTheThirdDigit(int theDesiredFigure)
+{
+    Console.WriteLine("Поиск третьей цифры в задданном числе");
+    Console.WriteLine("______________________________________");
+    Console.Write("Введите целое число: ");
+    string stringNum = Console.ReadLine()!;
+    if (stringNum.Length >= 3)
+    {
+        Console.WriteLine(theDesiredFigure + " цифра числа " + stringNum + " = " + stringNum[2]);
+    }
+    else
+    {
+        Console.WriteLine(theDesiredFigure + " цифры не существует");
+    }
+}
+searchForTheThirdDigit(3);
