df3a49976a485ab80c6937b3b77ca8a4236c5baf
Evgeniy Belousov <EvgSiteDev@yandex.ru>
2022-08-11 15:06:02 +0300

    выполнена задача номер 15

---
+++ task15/checkingTheWeekend.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Проверка номера дня недели на выходной");
+Console.WriteLine("______________________________________");
+Console.Write("Введите номер дня недели от 1 до 7: ");
+string stringDayNum = Console.ReadLine()!;
+int dayNum = int.Parse(stringDayNum);
+while (dayNum > 7 || dayNum == 0)
+{
+    if (dayNum > 7 || dayNum == 0)
+    {
+        Console.Write("Номер недели введен не корректно. Введите номер дня недели от 1 до 7: ");
+        stringDayNum = Console.ReadLine()!;
+        dayNum = int.Parse(stringDayNum);
+    }
+    else {
+        break;
+    }
+}
+if (dayNum == 6 || dayNum == 7)
+{
+    Console.Write("Данный день является выходным");
+}
+else
+{
+    Console.Write("Данный день является рабочим");
+}
+
