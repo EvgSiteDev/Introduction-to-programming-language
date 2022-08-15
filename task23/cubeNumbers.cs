33641702e24298894130dc2b6e1bed8ade295f25
Evgeniy Belousov <EvgSiteDev@yandex.ru>
2022-08-16 02:57:14 +0300

    Решена задача номер 23

---
+++ task23/cubeNumbers.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите целое число, до которого будет производится возведение в куб: ");
+
+int maxCubeNum = int.Parse(Console.ReadLine()!);
+string cubingFrom1ToN(int max)
+{
+    string cubeList = string.Empty;
+    for (int i = 1; i <= max; i++)
+    {
+        if (i != max)
+        {
+            cubeList = cubeList + Math.Pow(i, 3) + ", ";
+        }
+        else if (i == max)
+        {
+            cubeList = cubeList + Math.Pow(i, 3) + ".";
+        }
+    }
+    return cubeList;
+}
+Console.WriteLine(cubingFrom1ToN(maxCubeNum));
