a0a9f9fc48d8c2328ccf33ef22670eddba55026c
Evgeniy Belousov <EvgSiteDev@yandex.ru>
2022-08-17 00:54:16 +0300

    Выполнена задача номер 25. Удален файл README.

---
+++ task25/degreeOfNumber.cs
@@ -0,0 +1,30 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int dataEntry(string text)
+{
+    Console.Write(text + ": ");
+    return int.Parse(Console.ReadLine()!);
+};
+
+int num = dataEntry("Введите число");
+int degree = dataEntry("Введите степень");
+
+int exponentiation(int number, int degr)
+{
+    int res = 1;
+    for (int i = 1; i <= degr; i++)
+    {
+        res *= number;
+    }
+    return res;
+}
+
+void outputData(int number, int degreeOfNumber)
+{
+    Console.WriteLine(number + "^" + degreeOfNumber + " = " + exponentiation(num, degree));
+}
+outputData(num, degree);
