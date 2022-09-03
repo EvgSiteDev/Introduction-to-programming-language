05dd7e8a096f67d8a437d88e3aca8eb6fd0d3b97
Evgeniy Belousov <EvgSiteDev@yandex.ru>
2022-09-03 18:52:17 +0300

    Решена задача номер 68

---
+++ task68/Program.cs
@@ -0,0 +1,20 @@
+﻿int dataInput(string message)
+{
+dataValidation: Console.Write(message);
+    string introducedNumberString = Console.ReadLine()!;
+    if (!int.TryParse(introducedNumberString, out int introducedNumber) || introducedNumber < 0)
+    {
+        goto dataValidation;
+    }
+    return introducedNumber;
+}
+int akkermanFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0) return akkermanFunction(m - 1, 1);
+    else return akkermanFunction(m - 1, akkermanFunction(m, n - 1));
+}
+Console.WriteLine("Результат: " + akkermanFunction(dataInput("Введите m: "), dataInput("Введите n: ")));
